08e43a4200d4637679cd9cae0905ecd642f832aa
Magesh <tkmagesh77@gmail.com>
2017-07-14 12:28:06 +0530

    using two way 'ngModel' binding in calculator

--- first-app/src/app/calculator/calculatorOne.component.ts
+++ first-app/src/app/calculator/calculatorOne.component.ts
@@ -8,30 +8,22 @@ import { CalculatorModel } from './CalculatorModel';
 export class CalculatorOneComponent{
 	//result : number = 0;
 	calculator : CalculatorModel = new CalculatorModel();
-
-	onAddClick(value1, value2){
-		this.calculator.operand1 = parseInt(value1, 10);
-		this.calculator.operand2 = parseInt(value2, 10);
+	
+	onAddClick(){
+		
 		this.calculator.add();
-		//this.result = this.calculator.result;
 	}
-	onSubtractClick(value1, value2){
-		this.calculator.operand1 = parseInt(value1, 10);
-		this.calculator.operand2 = parseInt(value2, 10);
+	onSubtractClick(){
+		
 		this.calculator.subtract();
-		//this.result = this.calculator.result;
 	}
-	onMultiplyClick(value1, value2){
-		this.calculator.operand1 = parseInt(value1, 10);
-		this.calculator.operand2 = parseInt(value2, 10);
+	onMultiplyClick(){
+		
 		this.calculator.multiply();
-		//this.result = this.calculator.result;
 	}
-	onDivideClick(value1, value2){
-		this.calculator.operand1 = parseInt(value1, 10);
-		this.calculator.operand2 = parseInt(value2, 10);
+	onDivideClick(){
+		
 		this.calculator.divide();
-		//this.result = this.calculator.result;
 	}
 
 }
